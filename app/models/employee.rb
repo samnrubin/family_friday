8d462e74e5f8f1ad6cc84b7f4fbd98a57b5344ba
Sam Rubin <samnrubin@gmail.com>
2017-08-16 06:41:51 +0000

    family friday grouping model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,39 @@
 class Employee < ApplicationRecord
+  def self.groups
+    employees = Employee.all
+    return [employees] if employees.length < 6
+
+    employees = group_up(Employee.all.shuffle)
+
+    fix_stragglers(employees)
+  end
+
+  def self.group_up(employees)
+    employee_groups = [[]]
+
+    employees.each do |employee|
+      if employee_groups.last.length >= 4
+        employee_groups << []
+      end
+
+      employee_groups.last << employee
+    end
+
+    employee_groups
+  end
+
+  def self.fix_stragglers(employee_groups)
+    # add straggler to previous group if there's 1
+    if employee_groups.last.length == 1
+      odd = employee_groups.pop
+      employee_groups.last << odd.pop
+
+    # add employee from previous group to stragglers if there's 1
+    elsif employee_groups.last.length == 2
+      odd = employee_groups[-2].pop
+      employee_groups.last << odd
+    end
+
+    employee_groups
+  end
 end
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -4,4 +4,53 @@ class EmployeeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "There are no groups greater than 4 after a group_up call of 4" do
+    employees = %w( 1 2 3 4 5 6 )
+    grouped = Employee.group_up(employees)
+    grouped.each do |group|
+      assert group.length <= 4
+    end
+  end
+
+  test "There are no groups greater than 4 after a group_up call of 6" do
+    employees = %w( 1 2 3 4 5 6 )
+    grouped = Employee.group_up(employees)
+    grouped.each do |group|
+      assert group.length <= 4
+    end
+  end
+
+  test "There are no groups greater than 4 after a group_up call of 9" do
+    employees = %w( 1 2 3 4 5 6 7 8 9 )
+    grouped = Employee.group_up(employees)
+    grouped.each do |group|
+      assert group.length <= 4
+    end
+  end
+
+  test "There are no stragglers with a final group of 1" do
+    employees = [%w( 1 2 3 4 ), %w( 5 )]
+    grouped = Employee.fix_stragglers(employees)
+    grouped.each do |group|
+      assert group.length >= 3 && group.length <= 5
+    end
+  end
+
+  test "There are no stragglers with a final group of 2" do
+    employees = [%w( 1 2 3 4 ), %w( 5 6 )]
+    grouped = Employee.fix_stragglers(employees)
+    grouped.each do |group|
+      assert group.length >= 3 && group.length <= 5
+    end
+  end
+
+  test "There are no stragglers with a final group of 3" do
+    employees = [%w( 1 2 3 4 ), %w( 5 6 7 )]
+    grouped = Employee.fix_stragglers(employees)
+    grouped.each do |group|
+      assert group.length >= 3 && group.length <= 5
+    end
+  end
 end
